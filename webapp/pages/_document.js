5ee1505e053cdb51db8f41f0351c638bc73d47e0
AKT311209 <khoi@tran.com.vn>
2025-02-23 13:09:23 +0700

    Update _document.js to adjust asset paths and add viewport meta tags

--- webapp/pages/_document.js
+++ webapp/pages/_document.js
@@ -4,11 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap" />
-        <link rel="stylesheet" href="/bs-theme-overrides.css" />
-        <link rel="stylesheet" href="/Contact-Details-icons.css" />
-        <link rel="stylesheet" href="/Pretty-Search-Form-.css" />
+        <link rel="stylesheet" href="assets/css/bs-theme-overrides.css" />
+        <link rel="stylesheet" href="assets/css/Contact-Details-icons.css" />
+        <link rel="stylesheet" href="assets/css/Pretty-Search-Form-.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
       </Head>
       <body>
         <Main />
@@ -16,3 +18,4 @@ export default function Document() {
       </body>
     </Html>
   )
+}
