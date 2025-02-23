4e9611032b2ead1989524b500815a8f336c5b8d5
AKT311209 <khoi@tran.com.vn>
2025-02-23 13:07:13 +0700

    Add initial project structure and core files

---
+++ webapp/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ webapp/pages/Document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap" />
+        <link rel="stylesheet" href="/bs-theme-overrides.css" />
+        <link rel="stylesheet" href="/Contact-Details-icons.css" />
+        <link rel="stylesheet" href="/Pretty-Search-Form-.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
