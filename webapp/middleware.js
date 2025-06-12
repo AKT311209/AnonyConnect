373a11d6e57d519ae0e4e8af5eaf4edb98bd3352
AKT311209 <khoi@tran.com.vn>
2025-06-12 14:57:50 +0700

    feat: update document metadata and remove unused logo files for improved branding

--- webapp/middleware.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function middleware(req) {
-  // Only forward the request, do not check token or session here
-  return NextResponse.next();
-}
--- webapp/pages/_document.js
+++ webapp/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head data-bs-theme="light" lang="en">
+        <title>AnonyConnect</title>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap" />
         <link rel="stylesheet" href="/assets/css/bs-theme-overrides.css" />
