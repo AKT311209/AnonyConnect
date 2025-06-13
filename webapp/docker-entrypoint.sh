2094d4c5ae4a2829a119f20669871cefe1315bf5
AKT311209 <khoi@tran.com.vn>
2025-06-13 21:43:42 +0700

    fix: update Dockerfile and entrypoint script to remove build commands and adjust environment variables

--- webapp/docker-entrypoint.sh
+++ webapp/docker-entrypoint.sh
@@ -3,4 +3,11 @@
 if [ ! -f /app/storage/config.json ]; then
   cp /app/config.default.json /app/storage/config.json
 fi
+
+# If .next does not exist, run next build with current env vars
+if [ ! -d ".next" ]; then
+  echo "No .next directory found, running next build with current env vars..."
+  npm run build
+fi
+
 exec "$@"
